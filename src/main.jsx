585cde792e7fdd4ae85055fe264eff49118899fa
ceretoon <ceretoon@gmail.com>
2025-04-04 23:17:17 +0200

    Fix style.css path for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './style.css';
+import '../style.css';
 
 if (window.Telegram && window.Telegram.WebApp) {
   window.Telegram.WebApp.ready();
